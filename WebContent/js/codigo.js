4fcf085a6fbcc75def781f90ed6e5b13f071a5de
agusmar91 <34574688+agusmar91@users.noreply.github.com>
2017-12-20 12:39:53 +0100

    Update codigo.js

--- WebContent/js/codigo.js
+++ WebContent/js/codigo.js
@@ -5,12 +5,7 @@ function primo() {
 	var numero = parseInt(document.getElementById("txt").value);
 	var limite = Math.round(Math.sqrt(numero));
 	
-	if (numero == 1) {
-		
-		window.alert("HAS FALLADO!!\nTu puntuacion es de "+contador+" aciertos.");
-		document.location.href = "index.html";
-		
-	} else if (numero == 2) {
+	if (numero == 2||numero==1) {
 		
 		contador++;
 		window.alert("Es primo! LLevas un total de " + contador+ " aciertos!!");
@@ -19,7 +14,7 @@ function primo() {
 		
 		for (var i = 2; i <= limite; i++) {
 			
-			if (numero % i === 0 || numero == 1) {
+			if (numero % i === 0) {
 				window.alert("No es primo, conseguiste un total de " + contador+ " aciertos!!");
 				document.location.href = "index.html";
 				break;
